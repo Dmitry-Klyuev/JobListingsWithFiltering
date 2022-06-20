9c054f1279c6378fc889705fa970c1e9994d9ca5
Dmity-kl <i6169696@gmail.com>
2022-06-20 17:50:51 +0300

    add persist

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
-
-import {store} from './store'
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App/>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
